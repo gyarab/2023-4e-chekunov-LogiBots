5a13b27bb20a3f2c6393640f37cb42a99aaef11a
Egor <egor.chekunov@student.gyarab.cz>
2024-01-16 00:51:34 +0100

    backend init

---
+++ backend/logiback/global_info/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GlobalInfoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'global_info'
---
+++ backend/logiback/global_info/views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+
+from logiback.global_info.serializers import GroupSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+# Create your views here.
